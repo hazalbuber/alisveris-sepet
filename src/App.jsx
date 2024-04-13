2f464a05022f6e9dba5ae961be271b1d54cfd809
hazalbuber <91335675+hazalbuber@users.noreply.github.com>
2024-04-14 02:45:04 +0300

    Product and ProductItem components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import Product from "./components/Product"
 
 function App() {
   return (
     <div className="container mx-auto p-4">
       <Header />
+      <Product />
     </div>
   )
 }
