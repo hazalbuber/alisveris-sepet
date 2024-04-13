f6cc87ad34f304e1fadfb9fb5e934c784fddafe7
hazalbuber <91335675+hazalbuber@users.noreply.github.com>
2024-04-14 02:53:34 +0300

    Cart component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Product from "./components/Product"
+import Cart from "./components/Cart"
 
 function App() {
   return (
     <div className="container mx-auto p-4">
       <Header />
       <Product />
+      <Cart/>
     </div>
   )
 }
